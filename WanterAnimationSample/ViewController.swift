5a8dac7b396e998797a1266a50add4e81ba86aa4
432daiki <daiki.develop@gmail.com>
2018-05-30 07:36:16 +0900

    first comitt

---
+++ WanterAnimationSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WanterAnimationSample
+//
+//  Created by Daiki Shimizu on 2018/05/29.
+//  Copyright © 2018年 Daiki Shimizu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let animationLayer = ArcLayer()
+        animationLayer.frame = CGRect(x: 10, y: 50, width: 300, height: 300)
+        view.layer.addSublayer(animationLayer)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            animationLayer.animate()
+        }
+    }
+
+    
+}
+
